381bfc1e3887c224477a4330aa43c2094810070b
小虎12685 <tiger.wang@yunzhihui.com>
2019-01-27 10:14:08 +0800

    new

---
+++ README.md
@@ -0,0 +1,9 @@
+##第一步
+使用：https://pv.sohu.com/cityjson , 获取到ip地址;
+
+##第二步
+使用 JavaScript生成dom画出水印
+
+
+##依赖
+需要引入 <script src="https://pv.sohu.com/cityjson"></script>
---
+++ js/random_id.js
@@ -0,0 +1,7 @@
+processID = () => {
+  const uuid = 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+  });
+  return uuid;
+}
